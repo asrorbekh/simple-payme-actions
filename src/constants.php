eed9a0cfea92c2fe51104ad4aca52851c445d029
asrorbekh <asrorbek0325@gmail.com>
2023-10-04 00:32:41 +0500

    Rollback url scheme

--- src/constants.php
+++ src/constants.php
@@ -4,4 +4,4 @@ define("CORE", dirname(__FILE__, 2));
 
 const LOG_PATH = CORE . '/logs/';
 
-const PAYME_ENDPOINT = 'httpsw://checkout.test.paycom.uz/api';
+const PAYME_ENDPOINT = 'https://checkout.test.paycom.uz/api';
